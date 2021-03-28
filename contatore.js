0778ef5d714694e2886b36d0f8ef9a89a5abf9d5
Criss-b <76134184+Criss-b@users.noreply.github.com>
2021-03-28 17:08:10 +0200

    Add files via upload

---
+++ contatore.js
@@ -0,0 +1,68 @@
+let containerPara = document.createElement("div");
+document.body.append(containerPara);
+containerPara.className = "container-para";
+
+let para = document.createElement("p");
+containerPara.appendChild(para);
+para.className = "para";
+para.setAttribute("id", "cambia");
+para.textContent = "0";
+
+
+let containerBottoni = document.createElement("div");
+document.body.append(containerBottoni);
+containerBottoni.className = "container-bottoni";
+
+
+let piu = document.createElement("button");
+let meno = document.createElement("button");
+containerBottoni.prepend(piu);
+containerBottoni.append(meno);
+piu.setAttribute("id", "piu");
+piu.setAttribute("onclick", "incrementa()");
+piu.textContent = "+";
+meno.setAttribute("id", "meno");
+meno.setAttribute("onclick", "decrementa()");
+meno.textContent = "-";
+
+
+let containerReset = document.createElement("div");
+document.body.append(containerReset);
+containerReset.className = "container-reset";
+
+let azzera = document.createElement("p");
+containerReset.appendChild(azzera);
+azzera.setAttribute("id", "reset");
+azzera.setAttribute("onclick", "reset()");
+azzera.textContent = "RESET";
+
+
+
+
+let conteggio = 0;
+const cambia = document.getElementById("cambia");
+document.addEventListener ("click", incrementa);
+function incrementa() {
+    conteggio++;
+    cambia.innerHTML = conteggio;
+}
+
+
+
+document.addEventListener('click', decrementa);
+function decrementa(){
+    conteggio--;
+    cambia.innerHTML = conteggio;
+    if (conteggio<0) {
+        cambia.style.color = "red";
+
+    } else {
+        cambia.style.color = "white";
+    }
+
+}
+
+document.addEventListener("click", reset());
+function reset() {
+    conteggio = 0;
+}
